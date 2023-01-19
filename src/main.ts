b7c604cf35624d3c4e5300121bc0b374b591129d
INOUE Takuya <inouetakuya5@gmail.com>
2023-01-19 22:33:53 +0900

    Kill and yank

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,33 @@
-import { Editor, MarkdownView, Plugin } from 'obsidian'
+import { Editor, EditorPosition, MarkdownView, Plugin } from 'obsidian'
 
-// Remember to rename these classes and interfaces!
+export default class KillAndYankPlugin extends Plugin {
+  private killRing: string
 
-export default class MyPlugin extends Plugin {
   async onload() {
-    // This adds an editor command that can perform some operation on the current editor instance
     this.addCommand({
-      id: 'sample-editor-command',
-      name: 'Sample editor command',
+      id: 'kill',
+      name: 'Kill (Cut from the cursor position to the end of the line)',
+      hotkeys: [{ modifiers: ['Ctrl'], key: 'k' }],
       editorCallback: (editor: Editor, view: MarkdownView) => {
-        console.log(editor.getSelection())
-        editor.replaceSelection('Sample Editor Command')
+        const position: EditorPosition = editor.getCursor()
+        const line: string = editor.getLine(position.line)
+
+        const textToBeRetained = line.slice(0, position.ch)
+        const textToBeCut = line.slice(position.ch)
+
+        this.killRing = textToBeCut
+
+        editor.setLine(position.line, textToBeRetained)
+        editor.setCursor(position, position.ch)
+      },
+    })
+
+    this.addCommand({
+      id: 'yank',
+      name: 'Yank (Paste)',
+      hotkeys: [{ modifiers: ['Ctrl'], key: 'y' }],
+      editorCallback: (editor: Editor, view: MarkdownView) => {
+        editor.replaceSelection(this.killRing)
       },
     })
   }
