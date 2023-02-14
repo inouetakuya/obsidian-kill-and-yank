adc461f6295b4fe37afa13d44340fda49f79eedd
INOUE Takuya <inouetakuya5@gmail.com>
2023-02-14 11:49:34 +0900

    Revert "Revert "feat: Set mark and Kill region""

--- README.md
+++ README.md
@@ -6,17 +6,29 @@ Obsidian https://obsidian.md
 
 ## Usage
 
-### Kill
+### Kill line
 
 Cut from the cursor position to the end of the line.
 
 default: `Control + k`
+
+### Kill region
+
+Cut the selection.
+
+default: `Control + w`
 
 ### Yank
 
 Paste kill ring.
 
 default: `Control + y`
+
+### Set mark
+
+Toggle the start position of the selection.
+
+default: `Control + Space`
 
 ## Note
 
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { Editor, EditorPosition, MarkdownView, Plugin } from 'obsidian'
 
 export default class KillAndYankPlugin extends Plugin {
+  private editor: Editor
   private killRing: string
+  private mark: EditorPosition | null = null
 
   async onload() {
     this.addCommand({
-      id: 'kill',
-      name: 'Kill (Cut from the cursor position to the end of the line)',
+      id: 'kill-line',
+      name: 'Kill line (Cut from the cursor position to the end of the line)',
       hotkeys: [{ modifiers: ['Ctrl'], key: 'k' }],
       editorCallback: (editor: Editor, view: MarkdownView) => {
         const position: EditorPosition = editor.getCursor()
@@ -23,11 +25,35 @@ export default class KillAndYankPlugin extends Plugin {
     })
 
     this.addCommand({
+      id: 'kill-region',
+      name: 'Kill region (Cut the selection)',
+      hotkeys: [{ modifiers: ['Ctrl'], key: 'w' }],
+      editorCallback: (editor: Editor, view: MarkdownView) => {
+        this.killRing = editor.getSelection()
+        editor.replaceSelection('')
+      },
+    })
+
+    this.addCommand({
       id: 'yank',
       name: 'Yank (Paste)',
       hotkeys: [{ modifiers: ['Ctrl'], key: 'y' }],
       editorCallback: (editor: Editor, view: MarkdownView) => {
         editor.replaceSelection(this.killRing)
+      },
+    })
+
+    this.addCommand({
+      id: 'set-mark',
+      name: 'Set mark (Toggle the start position of the selection)',
+      hotkeys: [{ modifiers: ['Ctrl'], key: ' ' }],
+      editorCallback: (editor: Editor, view: MarkdownView) => {
+        if (this.mark) {
+          editor.setSelection(this.mark, editor.getCursor())
+          this.mark = null
+          return
+        }
+        this.mark = editor.getCursor()
       },
     })
   }
